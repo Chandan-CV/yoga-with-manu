bb80c5e4055a50435aba75b2b39858c392ae727a
Chandan <chandancvp08@gmail.com>
2024-01-02 12:31:56 +0530

    made the socials interactive

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,22 +1,53 @@
 <script>
-	import Icon from "@iconify/svelte";
-
+	import Icon from '@iconify/svelte';
 </script>
+
 <div class="w-full bg-dark-purple text-white font-nunito flex flex-col items-center p-10">
-<p class="text-3xl font-semibold mb-2">Contact us</p>
-<p>Phone: +91 9353625030</p>
-<p>email: manu1yoga@gmail.com</p>
-<div class="flex flex-row w-full sm:w-1/2 justify-evenly mt-5">
-    <Icon icon="mdi:instagram" class="text-white text-3xl cursor-pointer"/>
-    <Icon icon="mdi:facebook" class="text-white text-3xl cursor-pointer"/>
-    <Icon icon="mdi:youtube" class="text-white text-3xl cursor-pointer"/>
-    <Icon icon="simple-icons:x" class="text-white text-3xl cursor-pointer"/>
+	<p class="text-3xl font-semibold mb-2">Contact us</p>
+	<p>Phone: +91 9353625030</p>
+	<p>email: manu1yoga@gmail.com</p>
+	<div class="flex flex-row w-full sm:w-1/2 justify-evenly mt-5">
+		<a href="https://www.instagram.com/manuwithyoga?igsh=ZGNjOWZkYTE3MQ%3D%3D&utm_source=qr">
+			<Icon icon="mdi:instagram" class="text-white text-3xl cursor-pointer" />
+		</a>
+
+		<a href="https://www.facebook.com/yogawithmanu">
+			<Icon
+				icon="mdi:facebook"
+				class="text-white text-3xl cursor-pointer"
+							/>
+		</a>
+
+		<a href="https://www.youtube.com/channel/UCcvJ7BsaDleU0wQQ3sxCAwA">
+			<Icon
+				icon="mdi:youtube"
+				class="text-white text-3xl cursor-pointer"
+				
+			/>
+		</a>
+
+		<a href="https://twitter.com/yogawithmanu">
+			<Icon
+				icon="simple-icons:x"
+				class="text-white text-3xl cursor-pointer"
+				
+			/>
+		</a>
+
+		<a href="https://www.linkedin.com/in/manu-chidananda-10182a43/">
+			<Icon
+				icon="mdi:linkedin"
+				class="text-white text-3xl cursor-pointer"
+				
+			/>
+		</a>
+	</div>
+	<div class="w-full flex flex-col justify-center items-center">
+		<p class="font-nunito text-3xl mt-7">Yoga with Manu</p>
+		<p class="text-sm">© 2021 Yoga with Manu. All rights reserved.</p>
+	</div>
 </div>
-<div class="w-full flex flex-col justify-center items-center">
-    <p class="font-nunito text-3xl mt-7">Yoga with Manu</p>
-    <p class="text-sm ">© 2021 Yoga with Manu. All rights reserved.</p>
-</div>
-</div>
-<div class="w-full flex flex-row justify-center bottom-0 ">
-    built with <Icon icon="mdi:heart" class="text-dark-purple text-2xl"/> <a href="https://www.linkedin.com/in/chandan-cv-3320441b4/"> by Chandan</a>
+<div class="w-full flex flex-row justify-center bottom-0">
+	built with <Icon icon="mdi:heart" class="text-dark-purple text-2xl" />
+	<a href="https://www.linkedin.com/in/chandan-cv-3320441b4/"> by Chandan</a>
 </div>
